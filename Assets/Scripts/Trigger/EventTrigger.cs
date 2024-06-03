e61f4ebf9e78a0db1a33b770494b47385d939e99
Nathalie <xnhogenbergx@gmail.com>
2024-06-03 16:33:17 +0200

    Auto stash before rebase of "Sounds" onto "Main"

--- Assets/Scripts/Trigger/EventTrigger.cs
+++ Assets/Scripts/Trigger/EventTrigger.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EventTrigger : MonoBehaviour
 {
     public Animator anim;
+    private int _animNumber = 2;
 
     private void Start()
     {
-        //Debug.Log("subscribing AnimTrigger");
         GameEvents.current.OnAnimTriggerEnter += AnimTrigger;
     }
 
@@ -16,12 +17,9 @@ public class EventTrigger : MonoBehaviour
     {
         if (anim != null)
         {
-            PlayAnimation(2);
+            PlayAnimation(_animNumber);
+            _animNumber++;
             Debug.Log("play 1st anim from this player");
-        }
-        else
-        {
-            return;
         }
     }
 
