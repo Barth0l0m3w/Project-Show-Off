f1c0e80dc45876503ce36ddd6640e64c5e69f87b
Nathalie <xnhogenbergx@gmail.com>
2024-06-06 16:53:01 +0200

    trying to play sounds on sequence
    thread problems. mow put it in one file and just stop when the player hits the next trigger with the stop sound function

--- Assets/Scripts/Sounds/SoundTrigger.cs
+++ Assets/Scripts/Sounds/SoundTrigger.cs
@@ -1,37 +1,53 @@
+using System.Collections.Generic;
+using FMOD;
+using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Debug = UnityEngine.Debug;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 public class SoundTrigger : MonoBehaviour
 {
     [field: SerializeField] public EventReference eventPath { get; private set; }
-
-    //[EventRef][SerializeField] private string ;
-    [SerializeField] private Transform soundorigin;
+    [SerializeField] private Transform soundOrigin;
     [SerializeField] private int id;
+    private bool _isPlayingSound = false;
+    private EventInstance soundEvent;
+
+    private Queue<EventReference> soundQueue = new Queue<EventReference>();
+    private EventInstance currentSoundEvent;
 
     private void Start()
     {
         GameEvents.current.OnSoundTrigger += PlaySound;
+        
     }
 
     private void PlaySound(int id)
     {
-        if (!eventPath.IsNull)
+        if (id == this.id)
         {
-            if (id == this.id)
-            {
-                Debug.Log("playing sound");
-                var soundEvent = RuntimeManager.CreateInstance(eventPath);
-                soundEvent.set3DAttributes(RuntimeUtils.To3DAttributes(soundorigin.position));
-                soundEvent.start();
-                soundEvent.release();
-                //AudioManager.current.PlayOneShot(FMODEvents.current.bats, soundorigin.position);
-            }
+            
+            Debug.Log("playing sound");
+            soundEvent = RuntimeManager.CreateInstance(eventPath);
+            soundEvent.set3DAttributes(RuntimeUtils.To3DAttributes(soundOrigin.position));
+            soundEvent.start();
+            soundEvent.release();
+            
+            //AudioManager.current.PlayOneShot(FMODEvents.current.bats, soundOrigin.position);
         }
+
         else
         {
             Debug.LogWarning("Event path is empty! Please assign a valid FMOD event path.");
         }
+    }
+    
+    // todo: call it from an event that is placed om the trigger, it calls when a new trigger is hit
+    private void StopSound()
+    {
+        soundEvent.stop(STOP_MODE.ALLOWFADEOUT);
     }
 
     private void OnDisable()
