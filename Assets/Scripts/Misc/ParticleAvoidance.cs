e708a391619a4c30c606984a1a5758c4b22050b2
Bulancea Matei <67746218+Oberonjr@users.noreply.github.com>
2024-06-19 15:44:03 +0200

    Playtest tweak, particles added and math v1

--- Assets/Scripts/Misc/ParticleAvoidance.cs
+++ Assets/Scripts/Misc/ParticleAvoidance.cs
@@ -7,6 +7,8 @@ public class ParticleAvoidance : MonoBehaviour
 {
     [SerializeField] private Transform leftHand;
     [SerializeField] private Transform rightHand;
+    [SerializeField] private float avoidArea = 0.7f;
+    [SerializeField] private float updateVelocityRate = 5f;
 
     private ParticleSystem pSys;
 
@@ -22,17 +24,28 @@ public class ParticleAvoidance : MonoBehaviour
 
         for (int i = 0; i < currentAmount; i++)
         {
-            Vector3 closestHand;
-            if (Vector3.Distance(leftHand.position, particles[i].position) >
-                Vector3.Distance(rightHand.position, particles[i].position)) closestHand = leftHand.position;
-            else closestHand = rightHand.position;
+            Vector3 closestHand = Vector3.Distance(leftHand.position, particles[i].position) <
+                                  Vector3.Distance(rightHand.position, particles[i].position) 
+                                ? leftHand.position 
+                                : rightHand.position;
             
             Vector3 currentVelocity = particles[i].velocity;
             float distanceToClosestHand = Vector3.Distance(particles[i].position, closestHand);
-            Vector3 direction = Vector3.ProjectOnPlane(particles[i].position - closestHand, Vector3.up);
+            Vector3 direction = Vector3.ProjectOnPlane(particles[i].position - closestHand, Vector3.up).normalized;
+
+            if (distanceToClosestHand <= avoidArea)
+            {
+                float forceMultiplier = Mathf.Lerp(1f, 0.1f, distanceToClosestHand / avoidArea);
+                Vector3 avoidanceForce = direction * forceMultiplier * (avoidArea - distanceToClosestHand);
+                particles[i].velocity = Vector3.Lerp(currentVelocity, currentVelocity + avoidanceForce, Time.deltaTime * updateVelocityRate);
+            }
         }
         
         pSys.SetParticles(particles, currentAmount);
     }
-    
+
+    private void Update()
+    {
+        if(this.gameObject.activeSelf)AvoidHands();
+    }
 }
