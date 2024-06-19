f4cd22d8d0a56ed31abcedda0bf8cbb2e95c2139
Bulancea Matei <67746218+Oberonjr@users.noreply.github.com>
2024-06-19 14:32:22 +0200

    Math script

--- Assets/Scripts/Misc/ParticleAvoidance.cs
+++ Assets/Scripts/Misc/ParticleAvoidance.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ParticleAvoidance : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform leftHand;
+    [SerializeField] private Transform rightHand;
+
+    private ParticleSystem pSys;
+
+    private void Start()
     {
-        
+        pSys = GetComponent<ParticleSystem>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void AvoidHands()
     {
+        var particles = new ParticleSystem.Particle[pSys.main.maxParticles];
+        var currentAmount = pSys.GetParticles(particles);
+
+        for (int i = 0; i < currentAmount; i++)
+        {
+            Vector3 closestHand;
+            if (Vector3.Distance(leftHand.position, particles[i].position) >
+                Vector3.Distance(rightHand.position, particles[i].position)) closestHand = leftHand.position;
+            else closestHand = rightHand.position;
+            
+            Vector3 currentVelocity = particles[i].velocity;
+            float distanceToClosestHand = Vector3.Distance(particles[i].position, closestHand);
+            Vector3 direction = Vector3.ProjectOnPlane(particles[i].position - closestHand, Vector3.up);
+        }
         
+        pSys.SetParticles(particles, currentAmount);
     }
+    
 }
